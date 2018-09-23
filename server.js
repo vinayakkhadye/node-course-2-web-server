de39609a4ac3441ff1ed562986c91bf7ec3c5f40
vinayak khadye <vinayak.khadye@v2solutions.com>
2018-09-23 14:29:27 +0530

    package and server.js ready for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express   = require('express');
 const hbs       = require('hbs');
 const fs        = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -50,7 +50,7 @@ app.use((req,res,next)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log("server is up and running");
+app.listen(port,()=>{
+    console.log(`server is up and running on ${port}`);
 });
 
